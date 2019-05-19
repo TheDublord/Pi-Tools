51b830ed6a870a4e09392159836fae69e571b9b0
TheDublord <moltenmaniac@att.net>
2019-05-18 19:26:19 -0700

    Add files via upload

---
+++ pi-stress.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Produces load on all available CPU cores
+"""
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+def f(x):
+    while True:
+        x*x
+
+if __name__ == '__main__':
+    processes = cpu_count()
+    print '-' * 20
+    print 'Running load on CPU'
+    print 'Utilizing %d cores' % processes
+    print '-' * 20
+    pool = Pool(processes)
+    pool.map(f, range(processes))
